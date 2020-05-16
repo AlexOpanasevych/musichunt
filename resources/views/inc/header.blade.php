02a80adcd920fd031d6ba4c152d8d4103cf917f2
Anyone2324 <36279090+Anyone2324@users.noreply.github.com>
2020-05-16 21:26:27 +0300

    header

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,58 @@
+<header class="page-header-desktop">
+    <div class="header-column-1">
+        <div style="display: flex; flex-direction:row;">
+            <div class="header-logo"><img class="logo" src="images/logo.svg"></div>
+            <strong class="header-name">MUSIC<br>HUNT</strong>
+        </div>
+        <div class="search">
+            <div class="search-line">Пошук</div>
+            <div class="searching"><img src="images/magnifying-glass.svg" style="height: 40px;"></div>
+        </div>
+        <div>
+            <img class="icons" src="images/man-avatar.svg">
+            <img class="icons" src="images/love-and-romance.svg">
+            <img class="icons" src="images/commerce-and-shopping.svg">
+        </div>
+    </div>
+    <div class="header-navbar">
+        <a href="#sales">Знижки</a>
+        <a href="#sales">Новинки</a>
+        <a href="#sales">Гiтари</a>
+        <a href="#sales">Клавiшнi</a>
+        <a href="#sales">Духовi</a>
+        <a href="#sales">Смичковi</a>
+        <a href="#sales">Ударнi</a>
+        <a href="#sales">Аксесуари</a>
+        <a href="#sales">Бренди</a>
+    </div>
+</header>
+<header class="page-header-mob">
+        <nav role="navigation" style="width: 30%;">
+            <div id="nav-icon">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <a href="#"><li>Знижки</li></a>
+                    <a href="#"><li>Новинки</li></a>
+                    <a href="#"><li>Гiтари</li></a>
+                    <a href="#"><li>Клавiшнi</li></a>
+                    <a href="#"><li>Духовi</li></a>
+                    <a href="#"><li>Смичковi</li></a>
+                    <a href="#"><li>Ударнi</li></a>
+                    <a href="#"><li>Аксесуари</li></a>
+                    <a href="#"><li>Бренди</li></a>
+                </ul>
+            </div>
+        </nav>
+    <div style="display: flex; flex-direction:row;">
+        <div class="header-logo"><img class="logo" src="images/logo.svg"></div>
+        <strong class="header-name">MUSIC<br>HUNT</strong>
+    </div>
+    <div>
+        <img class="icons" src="images/man-avatar.svg">
+        <img class="icons" src="images/love-and-romance.svg">
+        <img class="icons" src="images/commerce-and-shopping.svg">
+    </div>
+</header>
