9e21453e567d0d1991ba50a77d504ff688473375
Anyone2324 <36279090+Anyone2324@users.noreply.github.com>
2020-05-20 14:36:28 +0300

    login + registry + info

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,13 +3,19 @@
 @section('page-content')
     <div class="login_content d-flex flex-column justify-content-center align-middle">
         <div class="login_tab container d-flex flex-column justify-content-center">
-            <h3 class="order_h3">Iм'я або пошта</h3>
-            <input type="text" size="40">
-            <h3 class="order_h3">Пароль</h3>
-            <input type="text" size="40">
-            <p><input type="checkbox" name="a" value="remember">Запам'ятати?</p>
+            <label for="nameoremail" class="order_h3">Iм'я або пошта</label>
+            <input class="input_text" name="nameoremail" type="text" size="40">
+            <label for="pass" class="order_h3">Пароль</label>
+            <input class="input_text" name="pass" type="text" size="40">
+            <div class="remember form-check">
+                <input type="checkbox" class="form-check-input" id="materialIndeterminate2" checked>
+                <label class="form-check-label" for="materialIndeterminate2">Запам'ятати?</label>
+            </div>
             <button type="button" class="login_button btn btn-outline-dark">Ввiйти</button>
-            <h3 class="order_h3" style="text-align: right; padding-bottom: 20px;">Забули пароль?</h3>
+            <div class="d-flex flex-row justify-content-between">
+                <h3 class="order_h3" style="text-align: right; padding-bottom: 20px;">Зареєструватися</h3>
+                <h3 class="order_h3" style="text-align: left; padding-bottom: 20px;">Забули пароль?</h3>
+            </div>
         </div>
     </div>
 @endsection
